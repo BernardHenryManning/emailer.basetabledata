a8b70f94f76ff26388d224e299868ba50ea43257
Crayonblack <craigtoit@gmail.com>
2014-02-04 12:15:53 +0200

    initial release

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-emailer.basetabledata
-=====================
+# emailer.basetabledata #
 
-Automatically create a basic table line that includes cellpadding='0' cellspacing='0' border='0' by using ctrl + shift + T
+An [Adobe Brackets](http://brackets.io) Automatically create a basic table line that includes cellpadding='0' cellspacing='0' border='0' by using ctrl + shift + T.
+
+# Installation #
+
+1. Open the Brackets Extension Manager and search for "Table".
+
+# Updates #
+
+* 2/04/2014 - Initial release
+
+# License #
+
+[The MIT License](LICENSE.md)
---
+++ main.js
@@ -0,0 +1,36 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global define, brackets */
+
+define(function () {
+    "use strict";
+    /*
+    Simple extension that adds a "File > New HTML Document" menu item
+    to insert a base table line at the cursor position
+    */
+    var CommandManager = brackets.getModule("command/CommandManager"),
+        EditorManager = brackets.getModule("editor/EditorManager"),
+        Menus = brackets.getModule("command/Menus");
+
+
+    function insertemailTabledata() {
+        /* Insert the table line */
+        // The Table line
+        var emailTabledata = '<table cellpadding="0" cellspacing="0" border="0">';
+
+        var editor = EditorManager.getFocusedEditor();
+        if (editor) {
+            // Insert the table line at the current cursor position
+            var insertionPos = editor.getCursorPos();
+            editor.document.replaceRange(emailTabledata, insertionPos);
+        }
+    }
+
+
+    // Register extension in the File menu
+    var EXTENSION_ID = "emailer.basetabledata";
+    CommandManager.register("New HTML Document", EXTENSION_ID, insertemailTabledata);
+
+    // Assign a keyboard shortcut
+    var theMenu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
+    theMenu.addMenuItem(EXTENSION_ID, "Ctrl-Shift-T");
+});
