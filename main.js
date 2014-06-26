1f088f69c74e6a036f175cb0f3a3ec8ed4adcf1b
BernardHenryManning <bernardmanning@Bernards-MacBook-Air.local>
2014-06-25 23:31:43 -0500

    Fixed tabing
    fixed tabs that were offset

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ define(function () {
         /* Insert the table line */
         // The Table line
         var emailTabledata = '<table cellpadding="0" cellspacing="0" border="0">'+
-        '\t\t<tbody>\n\t\t\t<tr>\n\t\t\t\t<td></td>\n\t\t\t\t</tr>\n\t\t\t</tbody>\n\t\t</table>';
+        '\n\t<tbody>\n\t\t<tr>\n\t\t\t<td></td>\n\t\t\t</tr>\n\t\t</tbody>\n\t</table>';
 
         var editor = EditorManager.getFocusedEditor();
         if (editor) {
